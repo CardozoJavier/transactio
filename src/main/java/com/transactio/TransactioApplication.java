8e12d5a5df26cc151cbe0d89b1d86d5b5e711489
Javier Cardozo <cardozojavier.c@gmail.com>
2025-05-04 17:59:09 +0200

    feat: Create TransactioApplication

---
+++ src/main/java/com/transactio/TransactioApplication.java
@@ -0,0 +1,12 @@
+package com.transactio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactioApplication.class, args);
+    }
+}
